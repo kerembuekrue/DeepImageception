51b2f0ce863b350b68a517088a0a6b85f48ac4cc
Kerem Bükrü <kerembuekrue@gmail.com>
2024-07-16 18:13:31 +0200

    minor changes

--- examples/example_cnn_data_from_keras.py
+++ examples/example_cnn_data_from_keras.py
@@ -10,16 +10,15 @@ train_images, test_images = train_images / 255.0, test_images / 255.0
 # Verify the data, plot the first 25 images from the training set
 class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
-plt.figure(figsize=(10, 10))
-for i in range(25):
-    plt.subplot(5, 5, i + 1)
+plt.figure(figsize=(8, 5))
+for i in range(18):
+    plt.subplot(3, 6, i + 1)
     plt.xticks([])
     plt.yticks([])
     plt.grid(False)
     plt.imshow(train_images[i])
-    # The CIFAR labels happen to be arrays, 
-    # which is why you need the extra index
     plt.xlabel(class_names[train_labels[i][0]])
+plt.tight_layout
 plt.show()
 
 # Create the convolutional base
